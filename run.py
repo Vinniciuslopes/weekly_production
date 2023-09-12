f3082921dd3afb2db6c2cec079a346af97ec8fc2
Vinniciuslopes <>
2023-09-12 09:27:32 +0000

    add function to collect data of the week

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def item_pick_data():
+    '''Function colects number of items picked per 3 hour  shift'''
+    print('Please enter the number of items picked last week.')
+    print('The data provided should be 5 numbers separed by commas.')
+    
+    print('Example: 23,50,60,40')
+
+    data_str = input('Please enter the data here:')
+    print(f'The data provided is  {data_str}')
+
+
+
+item_pick_data()   
+
+    
