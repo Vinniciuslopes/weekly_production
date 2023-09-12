56b51813f2d5b709e94e63ed9845e4b5a127dac6
Vinniciuslopes <>
2023-09-12 09:51:36 +0000

    add validation function

--- run.py
+++ run.py
@@ -8,7 +8,19 @@ def item_pick_data():
     data_str = input('Please enter the data here:')
     print(f'The data provided is  {data_str}')
 
+    item_data = data_str.split(',')
+    validate_data(item_data)
 
+
+def validate_data(values):
+    '''In the try.function turns strings into intergens'''
+    try:
+        if len(values) != 5:
+            raise ValueError(f'The data provided was not 5 numbers, it was {len(values)}')
+    except ValueError as e:
+        print(f'Ivalid data:{e} please try again')
+        
+    
 
 item_pick_data()   
 
